84bedea139a91b28d338d7d9a430ef055370b7be
Nurdanchik <nysanovnurdan33@gmail.com>
2024-02-20 22:51:33 +0600

    task7

---
+++ leetcode/reverseint.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MAX, INT_MIN = 2**31 - 1, -2**31
+
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+
+        reversed_x = 0
+        while x != 0:
+            pop = x % 10
+            x //= 10
+
+
+            if reversed_x > (INT_MAX - pop) // 10:
+                return 0
+
+            reversed_x = reversed_x * 10 + pop
+
+        return sign * reversed_x
+
+
+solution = Solution()
+print(solution.reverse(123))   
+print(solution.reverse(-123))   
+print(solution.reverse(120))    
